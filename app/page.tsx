240a0d7afdbda6e49705f02cf645c1227ac8639c
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-06-21 11:40:40 +0200

    modified:   .env modified:   app/page.tsx new file:   components/DisplayImage.tsx new file:   components/UploadWidget.tsx modified:   package-lock.json modified:   package.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import AudioPlayer from '@/components/AudioPlayer';
+import DisplayImage from '@/components/DisplayImage';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AudioPlayer />
+      
+      <DisplayImage />
     </main>
   );
 }
